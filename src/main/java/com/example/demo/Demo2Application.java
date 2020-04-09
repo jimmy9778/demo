9a8d568833e4eee636758e53a92d5cb6f074dd26
jimmy9778 <1742617674@qq.com>
2020-04-09 17:26:34 +0800

    变更项目

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@MapperScan("com.example.demo.mapper")
-public class DemoApplication {
+@EnableEurekaServer
+public class Demo2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Demo2Application.class, args);
 	}
+
 }
